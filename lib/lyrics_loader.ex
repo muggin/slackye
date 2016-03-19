fe0e4efb4680b68bc936953f086fb57a09fe2496
Alexander Juda <alexander.juda@estimote.com>
2016-03-19 12:15:51 +0100

    Tokenising and excluding stop words in lyrics module.

--- lib/lyrics_loader.ex
+++ lib/lyrics_loader.ex
@@ -1,6 +1,12 @@
 defmodule LyricsLoader do
+
   def stream_file(filename) do
     File.stream!(filename) |>
     CSV.decode
   end
+
+  def tokenise_and_exclude(text, stop_words) do
+    Gibran.Tokeniser.tokenise(text, exclude: stop_words)
+  end
+
 end
--- test/lyrics_loader_test.exs
+++ test/lyrics_loader_test.exs
@@ -6,4 +6,12 @@ defmodule LyricsLoaderTest do
     line = stream |> Enum.take(1)
     assert(line != nil, "Should load CSV stream")
   end
+
+  test "tokenising" do
+    lyric = "Took nothing from no man; man i'm my own man"
+    stop_words = ["no", "my"]
+    tokenised = LyricsLoader.tokenise_and_exclude(lyric, stop_words)
+    expected = ["took", "nothing", "from", "man", "man", "i'm", "own", "man"]
+    assert(tokenised == expected, "#{tokenised} != #{expected}")
+  end
 end
